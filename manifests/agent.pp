d6013673a494278a8c7965bdc976536ced814a9a
David Schmitt <david.schmitt@puppet.com>
2018-03-13 11:23:32 +0000

    Initial empty classes to install on server and agent

---
+++ manifests/agent.pp
@@ -0,0 +1,10 @@
+# resource_api::agent
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include resource_api::agent
+class resource_api::agent {
+}
---
+++ manifests/server.pp
@@ -0,0 +1,10 @@
+# resource_api::server
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include resource_api::server
+class resource_api::server {
+}
