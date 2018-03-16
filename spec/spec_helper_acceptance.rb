d9ffc095faa464ead88682ca1e1fae7da3083391
David Schmitt <david.schmitt@puppet.com>
2018-03-16 15:42:46 +0000

    Make provisioning step more resilient against different shapes of nodesets

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -3,16 +3,16 @@ require 'beaker/module_install_helper'
 require 'beaker/puppet_install_helper'
 
 def beaker_opts
-  # { debug: true, trace: true, acceptable_exit_codes: (0...256) }
-  { expect_failures: true, acceptable_exit_codes: (0...256) }
+  { debug: true, trace: true, expect_failures: true, acceptable_exit_codes: (0...256) }
+  # { expect_failures: true, acceptable_exit_codes: (0...256) }
 end
 
 RSpec.configure do |c|
   c.before :suite do
     unless ENV['BEAKER_provision'] == 'no'
       run_puppet_install_helper
-      install_module_on(master)
-      install_module_on(default)
+      install_module_on(hosts_as('master'))
+      install_module_on(hosts_as('default'))
       install_module_dependencies_on(hosts)
     end
   end
