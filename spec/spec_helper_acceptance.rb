b99e4b416c8e01d790ff011bc6aecadf46019de2
Dave Armstrong <david.armstrong@puppet.com>
2019-01-18 11:33:57 +0000

    (maint) Use beaker 4 for acceptance testing

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,3 +1,5 @@
+require 'beaker-pe'
+require 'beaker-puppet'
 require 'beaker-rspec'
 require 'beaker/module_install_helper'
 require 'beaker/puppet_install_helper'
@@ -17,6 +19,7 @@ RSpec.configure do |c|
   c.before :suite do
     unless ENV['BEAKER_provision'] == 'no'
       run_puppet_install_helper
+      configure_type_defaults_on(hosts)
       install_module_on(hosts_as('master'))
       install_module_on(hosts_as('default'))
       install_module_dependencies_on(hosts)
